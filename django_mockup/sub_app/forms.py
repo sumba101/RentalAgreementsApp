5c7e1ada3f90d7dba86c1680112e83f4c5653d84
sumba101 <sumanth.goku@gmail.com>
2021-05-07 13:27:50 +0530

    forms params

--- django_mockup/sub_app/forms.py
+++ django_mockup/sub_app/forms.py
@@ -1,29 +1,32 @@
 from django import forms
 
+
 class QuestionsForm( forms.Form ):
-    name_Of_The_Lesse = forms.CharField(label="Name of the Lesse",max_length=100)
-    name_Of_The_Lessor = forms.CharField(label="Name of the Lessor",max_length=100)
-    address_of_leased_residence = forms.CharField(label="Address of Leased Residence",max_length=100)
-    tenure_of_lease = forms.IntegerField(label="Tenure of the Lease",help_text="in number of Months")
-    date_of_signing = forms.DateField(label="Date of the Lease Signing",help_text="day/month/year",input_formats="%d/%m/%Y",widget=forms.DateInput)
+    name_Of_The_lesse = forms.CharField( label="Name of the Lesse", max_length=100 )
+    name_Of_The_lessor = forms.CharField( label="Name of the Lessor", max_length=100 )
+    lesse_child_of = forms.CharField( label="(Lesse) Son/Daughter of", max_length=100 )
+    lessor_child_of = forms.CharField( label="(Lessor) Son/Daughter of", max_length=100 )
+    address_of_leased_residence = forms.CharField( label="Address of Leased Residence", max_length=100 )
+    tenure_of_lease = forms.IntegerField( label="Tenure of the Lease", help_text="in number of Months" )
+    date_of_signing = forms.DateField( label="Date of the Lease Signing", help_text="day/month/year",
+                                       input_formats="%d/%m/%Y", widget=forms.DateInput )
 
-    rent_amount = forms.IntegerField( label="Rent Amount",help_text="monthly in Rs" )
-    security_depost = forms.IntegerField( label="Security Deposit",help_text="in Rs" )
-    maintenance_charge = forms.IntegerField( label="Maintenance Charges", help_text="monthly in Rs")
-    electricity_charge = forms.IntegerField( label="Electricity charges", help_text="monthly in Rs" )
-    period_of_vacating_notice = forms.IntegerField( label="Period of Notice before vacating",help_text="in Days" )
-    furnishing = forms.ChoiceField( label="Furnishing", widget=forms.RadioSelect,choices=[('1','Fully furnished'),('2','Semi furnished'),('3','Unfurnished')] )
+    rent_amount = forms.IntegerField( label="Rent Amount", help_text="monthly in Rs" )
+    due_day= forms.IntegerField(label="Day of month for rent due",help_text="in no.of days from month start")
+    security_deposit = forms.IntegerField( label="Security Deposit", help_text="in Rs" )
+    security_deposit_release_date = forms.DateField( label="Date of security deposit release", help_text="day/month/year",
+                                       input_formats="%d/%m/%Y", widget=forms.DateInput )
 
-# Name(s) of the lessee
-# Name of the lessor
-# Son/Daughter of data for Lessor and Lessee
-# Address of the residence to be given on lease
-# Tenure of lease
-# Date of signing
-# Rent amount
-# Security deposit and when it is released
-# Maintenance charges
-# Electricity charges
-# When would late fee be applicable, what mode of payment, how much will it be accrued
-# Period of notice before vacating premises
-# Fully furnished/Semi furnished/Unfurnished
+    maintenance_charge = forms.ChoiceField( label="Maintenance charges", widget=forms.RadioSelect,
+                                    choices=[('1', 'Lesse'), ('2', 'Lessor'), ('3', 'Included in Rent')] )
+
+    electricity_charge = forms.ChoiceField( label="Electricity charges", widget=forms.RadioSelect,
+                                    choices=[('1', 'Lesse'), ('2', 'Lessor'), ('3', 'Included in Rent')] )
+
+
+    period_of_vacating_notice = forms.IntegerField( label="Period of Notice before vacating", help_text="in Days" )
+
+    furnishing = forms.ChoiceField( label="Furnishing", widget=forms.RadioSelect,
+                                    choices=[('1', 'Fully furnished'), ('2', 'Semi furnished'), ('3', 'Unfurnished')] )
+    furnishing_present = forms.CharField( label="List Furnishing present in house and/or also description of house", max_length=200 )
+
